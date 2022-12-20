c77bd153e1d641e0bd904a5103cff17611005aaf
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-20 15:54:20 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightBox from 'simplelightbox';
 // Change code below this line
 
-console.log(galleryItems);
-console.log(SimpleLightBox);
+import SimpleLightBox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const markup = makeGalleryMarkup(galleryItems);
+
+function makeGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
