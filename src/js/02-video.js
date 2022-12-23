05be779f2db356466cf50688cd147a42a6fed9e1
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-23 19:57:09 +0200

    Completed  Task # 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,14 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-console.log(Player);
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  console.log(data.seconds);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
