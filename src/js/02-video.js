e53319e1c81b30951d3ee5bcd8d0a946fc2409ac
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-24 22:03:16 +0200

    update Task # 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,20 +2,20 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
+
+const KEY = 'videoplayer-current-time';
+let currentTime = JSON.parse(localStorage.getItem(KEY)) || 0;
+
 const player = new Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 playbackPosition();
 
-function onPlay(data) {
-  console.log(data.seconds);
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+function onPlay(currentTime) {
+  localStorage.setItem(KEY, JSON.stringify(currentTime.seconds));
 }
 
 function playbackPosition() {
-  const isTrue = localStorage.getItem('videoplayer-current-time');
-  if (isTrue) {
-    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-  }
+  player.setCurrentTime(currentTime);
 }
