1178b3acf7933d5d6a4348a1250a75ed91bc85cd
Pavlo Berkut <109954038+pberkut@users.noreply.github.com>
2022-12-25 14:10:42 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,19 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
-const iframe = document.querySelector('#vimeo-player');
+const iframe = document.querySelector("#vimeo-player");
 
-const KEY = 'videoplayer-current-time';
-let currentTime = JSON.parse(localStorage.getItem(KEY)) || 0;
+const KEY = "videoplayer-current-time";
+let currentTime = localStorage.getItem(KEY) || 0;
 
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(onPlay, 1000));
+player.on("timeupdate", throttle(onPlay, 1000));
 
 playbackPosition();
 
 function onPlay(currentTime) {
-  localStorage.setItem(KEY, JSON.stringify(currentTime.seconds));
+  localStorage.setItem(KEY, JSON.currentTime.seconds);
 }
 
 function playbackPosition() {
